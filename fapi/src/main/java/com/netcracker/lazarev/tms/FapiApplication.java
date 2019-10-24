fcdf140d54e15bcf12e557783a766c9dfe965118
NEONSik <47497875+NEONSik@users.noreply.github.com>
2019-10-24 23:30:08 +0300

    fix: fapi package

--- fapi/src/main/java/com/netcracker/lazarev/tms/FapiApplication.java
+++ fapi/src/main/java/com/netcracker/lazarev/tms/FapiApplication.java
@@ -1,4 +1,4 @@
-package com.netcracker.lazarev.tms.fapi;
+package com.netcracker.lazarev.tms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
