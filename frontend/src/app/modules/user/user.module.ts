08da0aae165c1ccca475940050c214ef59ec6a1c
NEONSik <47497875+NEONSik@users.noreply.github.com>
2019-11-08 17:13:18 +0300

    feat: work with UserService

--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UserService} from '../../services/user.service';
+import { UserTableComponent } from './components/user-table/user-table.component';
+import {MatTableModule} from '@angular/material';
+import {AppModule} from '../../app.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule
   ],
-    providers: [UserService]
+  exports: [
+
+  ],
+  providers: [UserService]
 })
 export class UserModule { }
