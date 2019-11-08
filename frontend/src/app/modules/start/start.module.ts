f760c868d342284f8888660609cc0af0067a951d
NEONSik <47497875+NEONSik@users.noreply.github.com>
2019-11-08 17:17:01 +0300

    fix: changed start-page companent

--- frontend/src/app/modules/start/start.module.ts
+++ frontend/src/app/modules/start/start.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartPageComponent } from './components/start-page/start-page.component';
+import {AppModule} from '../../app.module';
 
 
 
 @NgModule({
   declarations: [StartPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppModule
   ]
 })
 export class StartModule { }
