cd513ef9d03bd1a4f2de112f3a59edd96d8117cc
NEONSik <47497875+NEONSik@users.noreply.github.com>
2019-11-11 22:19:47 +0300

    feat: created new  components for popus

---
+++ frontend/src/app/modules/user/components/new-user/new-user.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../model/user';
+import {MatDialogRef} from '@angular/material';
+import {UserService} from '../../../../services/user.service';
+
+@Component({
+  selector: 'app-new-user',
+  templateUrl: './new-user.component.html',
+  styleUrls: ['./new-user.component.css']
+})
+export class NewUserComponent implements OnInit {
+  newUserForm: FormGroup;
+  newUser: User = new User();
+
+  constructor(public diologRef: MatDialogRef<NewUserComponent>, private userservice: UserService, private formBulder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.newUserForm = this.formBulder.group({
+      email: ['', [Validators.email]],
+      password: ['', [Validators.minLength(5)]],
+      role: ['', [Validators.required]]
+    });
+  }
+
+  close() {
+    this.diologRef.close();
+  }
+
+  send() {
+    this.newUser.email = this.newUserForm.controls.email.value;
+    this.newUser.password = this.newUserForm.controls.password.value;
+    this.newUser.role = this.newUserForm.controls.role.value;
+    this.userservice.createUser(this.newUser).subscribe(() => {
+      this.diologRef.close();
+    });
+  }
+}
